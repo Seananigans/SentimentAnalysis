2f93e75727e47fded099492a549aa75c012c2e70
Seananigans <sph99@comcast.net>
2016-07-07 23:42:36 -0400

    feat: Use Json library to retrieve
    feat: Use Json library to retrieve
    
    Updated text retrieval to use json library.

--- twitter_stream.py
+++ twitter_stream.py
@@ -11,13 +11,22 @@ class Listener(StreamListener):
 	
 	def on_data(self, data):
 		try:
-			tweet = data.split(',"text":"')[1]
-			tweet = tweet.split('","source"')[0]
+			# Get tweet by splitting data
+# 			tweet = data.split(',"text":"')[1]
+# 			tweet = tweet.split('","source"')[0]
+
+			# Get tweet with JSON library
+			all_data = json.loads(data)
+			tweet = all_data['text'].encode('utf-8')
+			
+			#save data
 			save_this = str(time.time())+"::::"+tweet
 			saveFile = open("twitDB.csv","a")
 			saveFile.write(save_this)
 			saveFile.write("\n")
 			saveFile.close()
+			
+			
 			return True
 		except BaseException, e:
 			print "Failed on_data,",str(e)
